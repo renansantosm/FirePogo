17a5e1f51098f5030477b7e2ef339f1399a319fa
renan <renan@DESKTOP-TPED91N>
2025-07-16 09:48:56 -0300

    Creating assert folder Inserting all folder Creating the menu: background,sound and fonts Creating the const file

---
+++ FirePogo/Code/Const.py
@@ -0,0 +1,11 @@
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME',
+               'EXIT')
+
+# W
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
---
+++ FirePogo/main.py
@@ -0,0 +1,4 @@
+from Code.Game import Game
+
+game = Game()
+game.run()
